129d8e22085e6a010cca32f269babe424e4b59a9
sachinmeesi324 <154306050+sachinmeesi324@users.noreply.github.com>
2024-01-04 18:55:33 +0530

    Create tomcatDeploy.groovy

---
+++ vars/tomcatDeploy.groovy
@@ -0,0 +1,6 @@
+def call(ipAddress,userName,credentialId,warName){
+  sshagent(['${credentialId}']) {
+                sh "scp -o StrictHostKeyChecking=no target/${warName} ${userName}@${ipAddress}:/opt/tomcat9/webapps/"
+                sh "ssh ${userName}@${ipAddress} /opt/tomcat9/bin/shutdown.sh"
+                sh "ssh ${userName}@${ipAddress} /opt/tomcat9/bin/startup.sh"
+}
